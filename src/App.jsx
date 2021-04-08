24cb7e8a3ef265d3a6e922ce555b3d6e68428d8c
Bo Opfer <boopfer9k9@gmail.com>
2021-04-08 12:29:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={styles.container}>
+        some stuff here that should be red
+
+
+      </div>
+      Its me! Nothing.
+    </div>
+  );
+}
+
+export default App;
